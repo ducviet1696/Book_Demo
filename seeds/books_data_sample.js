5c9b32537b19301f67440362341421fbd5237e57
ducviet1696 <ducviet169614@gmail.com>
2018-03-19 11:51:29 +0700

    ajax

--- seeds/books_data_sample.js
+++ seeds/books_data_sample.js
@@ -5,11 +5,11 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('books').insert([
-        {title: 'rowValue1', author: 'tac gia', publisher_id: 1, price: 12000},
-        {title: 'rowValue1', author: 'tac gia', publisher_id: 1, price: 12000},
-        {title: 'rowValue1', author: 'tac gia', publisher_id: 1, price: 12000},
-        {title: 'rowValue1', author: 'tac gia', publisher_id: 1, price: 12000},
-        {title: 'rowValue1', author: 'tac gia', publisher_id: 1, price: 12000}
+        {title: 'Dac Nhan Tam', author: 'Dale Carnegie', publisher_id: 2, price: 12000},
+        {title: 'Doi thay doi khi chung ta thay doi', author: 'Andrew Matthews', publisher_id: 1, price: 12000},
+        {title: 'Chang Hobbit', author: 'J.R.R Tolkien', publisher_id: 1, price: 12000},
+        {title: 'Cho toi xin mot ve di tuoi tho', author: 'Nguyen Nhat Anh', publisher_id: 1, price: 12000},
+        {title: 'Toi thay ho vang tren co xanh', author: 'Nguyen Nhat Anh', publisher_id: 3, price: 12000}
       ]);
     });
 };
--- seeds/publishers_data_sample.js
+++ seeds/publishers_data_sample.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('publishers').insert([
-        {id: 1, name: 'rowValue1', address: 'HN', phone:'0189'},
-        {id: 2, name: 'rowValue1', address: 'HN', phone:'0189'},
-        {id: 3, name: 'rowValue1', address: 'HN', phone:'0189'}
+        {id: 1, name: 'NXB Kim Dong', address: 'HN', phone:'0189'},
+        {id: 2, name: 'NXB Giao Duc', address: 'HN', phone:'0189'},
+        {id: 3, name: 'NXB Nha Nam', address: 'HN', phone:'0189'}
       ]);
     });
 };
